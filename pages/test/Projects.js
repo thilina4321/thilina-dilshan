e06293293d4679c5a97486a5a46fb13a270035c4
thilina_dilshan <thilina.dilshan@arimaclanka.com>
2022-03-01 19:09:25 +0530

    update projects

--- pages/test/Projects.js
+++ pages/test/Projects.js
@@ -12,25 +12,39 @@ const Projects = () => {
   const projects = [
     {
       id: 1,
-      image: "/chat.PNG",
-      title: "Chat Application",
-      url: "https://chat-node-socket-thilina1234.herokuapp.com",
-      
+      image: "/sampath-cms.png",
+      title: "Sampath Bank CMS (Industry project experience)",
+      url: "",
+      notView: true,
     },
     {
       id: 2,
+      image: "/sampath.png",
+      title: "Sampath Bank New Web (Industry project experience)",
+      url: "",
+      notView: true,
+    },
+    {
+      id: 3,
+      image: "/chat.PNG",
+      title: "Chat Application",
+      url: "https://chat-node-socket-thilina1234.herokuapp.com",
+    },
+    {
+      id: 4,
       image: "/disney.PNG",
       title: "Disney Clone",
       url: "https://react-disney-clone-dd5d8.web.app/home",
     },
     {
-      id: 3,
-      title: "Post share Application",
+      id: 5,
+      title: "Experience Application",
       url: "https://experience-share.vercel.app/",
       image: "/exe.PNG",
+      notView: true,
     },
     {
-      id: 4,
+      id: 6,
       title: "Weather app",
       url: "https://react-weather-app-ivb8m589h-thilina4321.vercel.app/",
       image: "/weather.PNG",
@@ -50,11 +64,18 @@ const Projects = () => {
           <div key={pro.id} className={classes.project}>
             <img src={pro.image} alt={pro.title} height={200} width={`100%`} />
             <h3> {pro.title} </h3>
-           { !pro.notView && <p>
-              <a href={pro.url} target="_blank" className={classes.a}>
-                View
-              </a>
-            </p>}
+            {!pro.notView && (
+              <p>
+                <a
+                  style={{ textDecoration: "none" }}
+                  href={pro.url}
+                  target="_blank"
+                  className={classes.a}
+                >
+                  View
+                </a>
+              </p>
+            )}
           </div>
         ))}
       </div>
--- pages/test/Quotes.js
+++ pages/test/Quotes.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { Fragment, useEffect, useState } from "react";
 import classes from "./quotes.module.css";
 import AOS from "aos";
 import "aos/dist/aos.css";
@@ -8,31 +8,26 @@ const Quotes = () => {
     AOS.init();
     AOS.refresh();
   }, []);
+  const [number, setNumber] = useState(0);
   const quotes = [
     {
       name: "Sudhara Dhananjaya",
       position: "Intern QA Engineer @ Virtusa",
-      quotes: (
-        <h2>
-          <bold> " </bold> Thilina is One of the best person I have ever work
+      quotes: ` " Thilina is One of the best person I have ever work
           with. His programming knowledge in javascript is very good and very
           good in MERN stack development. He is very good team player and I have
-          work with him directly. <bold> " </bold>
-        </h2>
-      ),
+          work with him directly."`,
     },
     {
       name: "Ishara Dilshan",
       position: "Software Engineer @ Arimac",
-      quotes: (
-        <h2>
-          <bold> " </bold> I have directly work with thilina in the industry
+      quotes: `
+          " I have directly work with thilina in the industry
           projects. He is very good team player with sounds on knowledge about
           REACT, NEXT, NODE. Quality Code is one of the most highlited feature
-          that I recognize from him when I worked with him at Arimac.{" "}
-          <bold> " </bold>
-        </h2>
-      ),
+          that I recognize from him when I worked with him at Arimac."
+          
+      `,
     },
   ];
   return (
@@ -46,9 +41,11 @@ const Quotes = () => {
             className={classes.quote}
             key={i}
           >
-            {q.quotes}
-            <h3> {q.name} </h3>
-            <h5> {q.position} </h5>
+            {number === i && <h2> {q.quotes} </h2>}
+            <div className={classes.sub} onClick={() => setNumber(i)}>
+              <h3> {q.name} </h3>
+              <h5> {q.position} </h5>
+            </div>
           </div>
         ))}
       </div>
